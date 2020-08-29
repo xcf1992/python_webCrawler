8ca9683c2655b53aa2df933c9ebd93ac3b61cd12
chenfu.xie <chenfu.xie@bytedance.com>
2020-08-28 20:12:04 -0700

    add download delay

--- xiuren/xiuren/pipelines.py
+++ xiuren/xiuren/pipelines.py
@@ -30,7 +30,7 @@ class XiurenPipeline(object):
         return item
 
     def save_image(self, name, link):
-        print(f"[save_image] {name}")
+        # print(f"[save_image] {name}")
         headers = {
             'user-agent' : "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36",
             'referer': 'http://www.xiuren.org/'
